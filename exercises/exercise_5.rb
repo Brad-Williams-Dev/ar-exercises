2eb4412181d118b246a298731e899c7a39e5ff4c
Brad Williams <bradley_williams2003@hotmail.com>
2023-01-23 17:07:01 -0400

    Exercise 5 completed

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@annual_revenue_sum =  Store.sum(:annual_revenue)
+
+print "The total revenue for all stores is : $#{@annual_revenue_sum}"
